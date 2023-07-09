5517e554b9fe3f8820bbcccc16cb98f58afea62e
Trevor Slawnyk <tblue1994@hotmail.com>
2023-07-09 04:12:47 +0000

    feat: implement day 1

---
+++ AdventOfCode.Solutions/Year2017/Day18/Solution.cs
@@ -0,0 +1,16 @@
+namespace AdventOfCode.Solutions.Year2017.Day18;
+
+class Solution : SolutionBase
+{
+    public Solution() : base(18, 2017, "") { }
+
+    protected override string SolvePartOne()
+    {
+        return "";
+    }
+
+    protected override string SolvePartTwo()
+    {
+        return "";
+    }
+}
---
+++ AdventOfCode.Solutions/Year2017/Day20/Solution.cs
@@ -0,0 +1,16 @@
+namespace AdventOfCode.Solutions.Year2017.Day20;
+
+class Solution : SolutionBase
+{
+    public Solution() : base(20, 2017, "") { }
+
+    protected override string SolvePartOne()
+    {
+        return "";
+    }
+
+    protected override string SolvePartTwo()
+    {
+        return "";
+    }
+}
